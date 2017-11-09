bbfe4314e1b12e1ff26129d576b925e50c2724c4
Daniel Jauregui <djauregui@csumb.edu>
2017-11-09 04:04:42 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Description:
+    This Project deploys a web app that has the basic operations of a chat room.
+
+Tools Using:
+    Render client-side views using React, JSX, and Javascript
+    
+ Known Problems:
+    Hard time Deploying due to build pack not being detected.
+   
+ How to Improve:
+    Have a user tracking system (possible geolocation tracking)
+
+   
---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// var usernames = ["user1","user2","user3","user4", "user5", "user6", "user7"];
+// var usernames = [];
+// var name = '';
+// var i = 0;
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+// function pageScroll() {
+//     window.scrollBy(0,1);
+//     scrolldelay = setTimeout(pageScroll,10);
+// }
+io.on('connection', function(socket){
+  // console.log(socket.username);
+  // name = prompt("Enter Username: ");
+  // usernames.push(name);
+  // console.log(usernames[i]);
+  socket.on('chat message', function(msg){
+    io.emit('chat message', "user: "+msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
