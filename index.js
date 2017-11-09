6f8566c6ea7c2260adfe6eee1db753067ed17fd7
Daniel Jauregui <djauregui@csumb.edu>
2017-11-09 04:33:17 +0000

    tried fixing port problem for deployment

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(process.env.PORT || 3000,, function(){
+  console.log('listening on *:3000');
 });
